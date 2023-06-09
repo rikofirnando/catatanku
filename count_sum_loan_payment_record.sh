33e14b637462a94c91640596a8702182a706ce6e
Riko Firnando <79784296+rikofirnando@users.noreply.github.com>
2023-06-09 23:27:42 +0700

    Create count_sum_loan_payment_record.sh

---
+++ count_sum_loan_payment_record.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Current Date & Time
+current_date=$(date "+%Y-%m-%d %T")
+echo -e "DATE NOW:\n$current_date"
+
+echo "HASIL COUNT & SUM LOAN_RECORD & PAYMENT_RECORD TABLES:"
+
+# Array
+tables=("loan_record" "payment_record")
+
+for table in "${tables[@]}"; do
+    result=$(/apps/mysql/mysql-comm-8.0.22/bin/mysql -h10.59.100.34 -P3448 -u loena loenadb -pB3rsAMaL0eN4#2021 -e "select count(*), sum(value) from $table;" 2>/dev/null)
+    count_tables=$(echo "$result" | awk 'NR==2')
+    echo "TOTAL $table: $count_tables"
+done
