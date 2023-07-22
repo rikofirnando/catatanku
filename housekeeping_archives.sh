78db9a692d0671ca603c13b1fa51e6d9d3585f3c
Riko Firnando <79784296+rikofirnando@users.noreply.github.com>
2023-07-22 16:55:11 +0700

    Update housekeeping_archives.sh

--- housekeeping_archives.sh
+++ housekeeping_archives.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Date
+# Current Date.
 current_date=$(date "+%Y-%m-%d %T")
 
 # The directory to be monitored.
@@ -16,10 +16,8 @@ archive_logs() {
 
 # Function to retrieve the percentage of storage usage in the directory.
 get_storage_usage_percentage() {
-  used=$(df -P "$1" | awk 'NR==2 {print $3}')
-  total=$(df -P "$1" | awk 'NR==2 {print $2}')
-  percentage=$((used * 100 / total))
-  echo $percentage
+  total_percentage=$(df -h $1 | awk 'NR==2 {print $5}' | awk -F "%" '{print $1}')
+  echo $total_percentage
 }
 
 # Result Before storage usage percentage
@@ -29,22 +27,22 @@ current_percentage_before=$(get_storage_usage_percentage "$target_directory")
 get_storage_usage_before=$(df -ah "$target_directory")
 
 # Displays human-unreadable storage status prior to archive execution.
-get_storage_usage_before_v2=$(df -P "$target_directory" | awk 'NR==2 {print "Storage Used in Byte(s):", $3}')
+get_storage_usage_before_v2=$(df -P "$target_directory" | awk 'NR==2 {print "Storage Used in KiloByte(s):", $3}')
 
 # Check the percentage of storage usage prior to execution.
 if [ "$current_percentage_before" -gt "$threshold_percentage" ]; then
   echo "Storage usage percentage: $current_percentage_before%"
-  echo "Storage usage is above ${threshold_percentage}%. Archiving *.log and/or *.txt files..."
+  echo "Storage usage is above or more than ${threshold_percentage}%. Archiving *.log and/or *.txt files..."
   archive_logs "$target_directory"
   echo "The archiving process is completed."
 else
   echo "Storage usage percentage: $current_percentage_before%"
-  echo "Storage usage is below ${threshold_percentage}%. No archiving needed."
+  echo "Storage usage is below or less than ${threshold_percentage}%. Tidak perlu pengarsipan."
 fi
 
-# Get the storage usage after archiving
+# Get the storage usage after archiving.
 current_percentage_after=$(get_storage_usage_percentage "$target_directory")
-current_percentage_after_v2=$(df -P "$target_directory" | awk 'NR==2 {print "Storage Used in Byte(s):", $3}')
+current_percentage_after_v2=$(df -P "$target_directory" | awk 'NR==2 {print "Storage Used in KiloByte(s):", $3}')
 
 # Displays date and time, along with storage status, after archive execution.
 echo "                       "
@@ -52,12 +50,13 @@ echo "Date Now: $current_date"
 echo "Storage status"
 echo "A. Human Unreadable"
 echo "1. Before archiving: "
-echo "$get_storage_usage_before_v2 = $current_percentage_before%"
+echo "${get_storage_usage_before_v2} KB = $current_percentage_before%"
 echo "2. After archiving: "
-echo "$current_percentage_after_v2 = $current_percentage_after%"
+echo "${current_percentage_after_v2} KB = $current_percentage_after%"
 echo "                       "
 echo "B. Human Readable"
 echo "1. Before archiving: "
 echo "$get_storage_usage_before"
 echo "2. After archiving: "
 echo "$(df -ah "$target_directory")"
+
