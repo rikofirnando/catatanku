b6421dec4295de4e3b0900d660635f264a3806ac
Riko Firnando <79784296+rikofirnando@users.noreply.github.com>
2023-07-23 07:29:40 +0700

    Update How to Archive Files.sh

--- How to Archive Files.sh
+++ How to Archive Files.sh
@@ -1,3 +1,4 @@
 find /data/ -name "*.log" -not -name "*.log.gz" -exec gzip -v {} \;
 find /data/ -name "*.log" -print -o -name "*.log.gz" -prune | xargs gzip -v
 find /data/ \( -name "*.log" -o -name "*.txt" \) -print -o \( -name "*.log.gz" -o -name "*.txt.gz" \) -prune | xargs gzip -v
+find /data/ \( -name "*.log" -o -name "*.txt" -o -name "*.ctl" \) -print -o \( -name "*.log.gz" -o -name "*.txt.gz" \) -o \( -name "LEDGER_SNAPSHOT_*.log" -o -name "LEDGER_SNAPSHOT_*.ctl" \) -prune | xargs gzip -v
