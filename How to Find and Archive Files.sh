377f144579de5fda2e158056b48e351877f93675
Riko Firnando <79784296+rikofirnando@users.noreply.github.com>
2023-07-23 10:06:03 +0700

    Update and rename How to Archive Files.sh to How to Find and Archive Files.sh

--- How to Find and Archive Files.sh
+++ How to Find and Archive Files.sh
@@ -2,3 +2,5 @@ find /data/ -name "*.log" -not -name "*.log.gz" -exec gzip -v {} \;
 find /data/ -name "*.log" -print -o -name "*.log.gz" -prune | xargs gzip -v
 find /data/ \( -name "*.log" -o -name "*.txt" \) -print -o \( -name "*.log.gz" -o -name "*.txt.gz" \) -prune | xargs gzip -v
 find /data/ \( -name "*.log" -o -name "*.txt" -o -name "*.ctl" \) -print -o \( -name "*.log.gz" -o -name "*.txt.gz" \) -o \( -name "LEDGER_SNAPSHOT_*.log" -o -name "LEDGER_SNAPSHOT_*.ctl" \) -prune | xargs gzip -v
+find /data/ \( -name "*.log" -o -name "*.txt" -o -name "*.ctl" -o -name "*.log.gz" -o -name "*.txt.gz" -o -name "LEDGER_SNAPSHOT_*.log" -o -name "LEDGER_SNAPSHOT_*.ctl" \) -print | grep -v -E "LEDGER_SNAPSHOT_.*\.(log|ctl)$" | xargs gzip -v
+find /data/ \( -name "*.log" -o -name "*.txt" -o -name "*.ctl" -o -name "*.log.gz" -o -name "*.txt.gz" -o -name "LEDGER_SNAPSHOT_*.log" -o -name "LEDGER_SNAPSHOT_*.ctl" \) -print | grep -v -E "LEDGER_SNAPSHOT_.*\.(log|ctl)$" | xargs gzip -v
